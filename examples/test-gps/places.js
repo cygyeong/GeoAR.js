6167d3312797d28373b0ced5ddb8e683719e366a
cyang <cyellowgreen@gmail.com>
2023-03-17 10:07:47 +0900

    update location

--- examples/test-gps/places.js
+++ examples/test-gps/places.js
@@ -1,78 +1,101 @@
-const loadPlaces = function(coords) {
-  // fetch data from user coords using external APIs, or simply add places data statically
-  // please look at GeoAR.js repository on examples/click-places/places.js for full code
-  const PLACES = [
-    {
-        name: "SSAFY",
-        location: {
-            lat: 35.20525, // add here latitude if using static data
-            lng: 126.81173, // add here longitude if using static data
+const loadPlaces = function (coords) {
+    // fetch data from user coords using external APIs, or simply add places data statically
+    // please look at GeoAR.js repository on examples/click-places/places.js for full code
+    const PLACES = [
+        {
+            name: "SSAFY",
+            location: {
+                lat: 35.20525, // add here latitude if using static data
+                lng: 126.81173, // add here longitude if using static data
+            },
+        },
+        {
+            name: "SSAFY",
+            location: {
+                lat: 35.20525, // add here latitude if using static data
+                lng: 126.81173, // add here longitude if using static data
+            },
+        },
+        {
+            name: "SSAFY",
+            location: {
+                lat: 35.2051, // add here latitude if using static data
+                lng: 126.8117, // add here longitude if using static data
+            },
+        },
+        {
+            name: "SSAFY",
+            location: {
+                lat: 35.2041, // add here latitude if using static data
+                lng: 126.8117, // add here longitude if using static data
+            },
+        },
+    ];
 
-        }
-    },
-];
-
-if (method === 'api') {
-    return loadPlaceFromAPIs(coords);
-}
-
-return Promise.resolve(PLACES);
-
-}
+    return Promise.resolve(PLACES);
+};
 
 window.onload = () => {
-    const scene = document.querySelector('a-scene');
+    const scene = document.querySelector("a-scene");
 
     // first get current user location
-    return navigator.geolocation.getCurrentPosition(function (position) {
-
-        // than use it to load from remote APIs some places nearby
-        loadPlaces(position.coords)
-            .then((places) => {
+    return navigator.geolocation.getCurrentPosition(
+        function (position) {
+            // than use it to load from remote APIs some places nearby
+            loadPlaces(position.coords).then((places) => {
                 places.forEach((place) => {
                     const latitude = place.location.lat;
                     const longitude = place.location.lng;
 
                     // add place icon
-                    const icon = document.createElement('a-image');
-                    icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    icon.setAttribute('name', place.name);
-                    icon.setAttribute('src', '../assets/map-marker.png');
+                    const icon = document.createElement("a-image");
+                    icon.setAttribute(
+                        "gps-entity-place",
+                        `latitude: ${latitude}; longitude: ${longitude};`
+                    );
+                    icon.setAttribute("name", place.name);
+                    icon.setAttribute("src", "../assets/map-marker.png");
 
                     // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-                    icon.setAttribute('scale', '20, 20');
+                    icon.setAttribute("scale", "20, 20");
 
-                    icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+                    icon.addEventListener("loaded", () =>
+                        window.dispatchEvent(
+                            new CustomEvent("gps-entity-place-loaded")
+                        )
+                    );
 
-                    const clickListener = function(ev) {
+                    const clickListener = function (ev) {
                         ev.stopPropagation();
                         ev.preventDefault();
-            
-                        const name = ev.target.getAttribute('name');
-            
-                        const el = ev.detail.intersection && ev.detail.intersection.object.el;
-            
+
+                        const name = ev.target.getAttribute("name");
+
+                        const el =
+                            ev.detail.intersection &&
+                            ev.detail.intersection.object.el;
+
                         if (el && el === ev.target) {
-                            const label = document.createElement('span');
-                            const container = document.createElement('div');
-                            container.setAttribute('id', 'place-label');
+                            const label = document.createElement("span");
+                            const container = document.createElement("div");
+                            container.setAttribute("id", "place-label");
                             label.innerText = name;
                             container.appendChild(label);
                             document.body.appendChild(container);
-            
+
                             setTimeout(() => {
                                 container.parentElement.removeChild(container);
                             }, 1500);
                         }
                     };
-            
-                    icon.addEventListener('click', clickListener);
+
+                    icon.addEventListener("click", clickListener);
 
                     scene.appendChild(icon);
                 });
-            })
-    },
-        (err) => console.error('Error in retrieving position', err),
+            });
+        },
+        (err) => console.error("Error in retrieving position", err),
         {
             enableHighAccuracy: true,
             maximumAge: 0,
